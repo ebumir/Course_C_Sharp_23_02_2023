3faa8d5fd33560d5188012e7997ecb25d59a5f76
Denis <denistokarev95.irk@gmail.com>
2023-02-24 16:49:33 +0800

    task 1_0 ver 01

--- Lesson1/1_0/Program.cs
+++ Lesson1/1_0/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+string s = Console.ReadLine()!;
+int num = int.Parse(s);
+
+
+
+Console.WriteLine(num * num);
