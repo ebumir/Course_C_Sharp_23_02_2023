bec256e6313da32d2b12d0754eec18b265dc75e2
Denis <denistokarev95.irk@gmail.com>
2023-02-24 17:32:50 +0800

    task 1_1

---
+++ Lesson1/1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число:");
+int a = int.Parse( Console.ReadLine()! );
+Console.WriteLine("Введите число являющееся его квадратом:");
+int b = int.Parse( Console.ReadLine()!);
+if (b == a * a)
+{
+    Console.WriteLine("Yes!");
+
+}
+else
+{
+    Console.WriteLine("NO!");
+}
