d4760820c1c8a43c3dfffaff96a2e353a1f07c4d
Denis <denistokarev95.irk@gmail.com>
2023-03-01 18:52:41 +0800

    Исправил первое задание

--- HomeWork_1/1_1/Program.cs
+++ HomeWork_1/1_1/Program.cs
@@ -3,11 +3,19 @@ Console.WriteLine("Введите первое число:");
 int a = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите второе число:");
 int b = int.Parse(Console.ReadLine()!);
+
 if (a>b)
 {
     Console.WriteLine( $" Max = {a}; Min = {b}");
 }
-else 
+else if (a <b)
 {
     Console.WriteLine( $" Max = {b}; Min = {a}" );
+}
+else 
+{
+
+Console.WriteLine("Ошибка! числа равны!");
+
+
 }
