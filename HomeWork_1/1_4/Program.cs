e513c61f2c93b028227fe883e34841e3e59372a2
Denis <denistokarev95.irk@gmail.com>
2023-03-01 20:06:52 +0800

    Добавил 4 задание

---
+++ HomeWork_1/1_4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Здравствуйте Мария!");
+Console.WriteLine("Введите число!");
+int a = int.Parse(Console.ReadLine()!);
+int b = -a;
+if ((a % 2)==0)
+{
+    while (b<=a-4)
+    {
+        Console.Write($"{b+2}, ");
+        b = b+2;
+    }
+}
+else 
+{
+    b= -a-1;
+    while (b<=a-2)
+    {
+       
+        Console.Write($"{b+2}, ");
+        b = b+2;
+    }
+}
+Console.WriteLine("");
+Console.WriteLine("Мария, Я очень вам благодарен, с вами приятно Работать!");
