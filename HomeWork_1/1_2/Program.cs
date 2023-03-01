f8dbdaed55482da2a362c6fc33d6b9f321dd0878
Denis <denistokarev95.irk@gmail.com>
2023-03-01 19:03:42 +0800

    Добавил второе задание

--- HomeWork_1/1_2/Program.cs
+++ HomeWork_1/1_2/Program.cs
@@ -1,6 +1,24 @@
-﻿int a = 8;
-int b = 6;
-int c = 11;
+﻿Console.WriteLine("Здравствуйте Мария!");
+Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число:");
+int c = int.Parse(Console.ReadLine()!);
 
+int max=a;
 
+if (b>max)
+{
+    max=b;
+}
+if (c>max)
+{
+    max=c;
+}
+else 
+{
+    Console.WriteLine("Числа равны!");
+}
+ Console.WriteLine($"max = {max}");
 
