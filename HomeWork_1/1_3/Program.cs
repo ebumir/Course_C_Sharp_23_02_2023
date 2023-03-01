d656f056bae405bd7fe1a7f027e19fb17a9f4a77
Denis <denistokarev95.irk@gmail.com>
2023-03-01 19:44:02 +0800

    Добавил третье задание

---
+++ HomeWork_1/1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Здравствуйте Мария!");
+Console.WriteLine("Введите число");
+int a = int.Parse (Console.ReadLine()!);
+
+if ((a % 2) == 0 )
+{
+    Console.WriteLine("Yes!");
+}
+
+else
+{
+    Console.WriteLine("No!");
+}
