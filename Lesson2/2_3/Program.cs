9f61fba2d3903843f172806f1f8ebb645c31c23b
Denis <denistokarev95.irk@gmail.com>
2023-03-03 17:50:27 +0800

    ClassWork 2_3 added

---
+++ Lesson2/2_3/Program.cs
@@ -0,0 +1,13 @@
+﻿void Div(int a, int b)
+{
+    if (a % b == 0)
+        Console.WriteLine("Кратно!");
+    else
+        Console.WriteLine("Некратно! Остаток: " + (a % b));
+}
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine()!);
+
+Div(a,b);
