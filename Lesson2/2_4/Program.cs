cce5b230019df8f5cab0cfaee378d3d8bdd4a851
Denis <denistokarev95.irk@gmail.com>
2023-03-03 19:01:33 +0800

    ClassWork 2_4 added

---
+++ Lesson2/2_4/Program.cs
@@ -0,0 +1,11 @@
+﻿void Div(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+        Console.WriteLine("Кратно!");
+    else
+        Console.WriteLine("Не кратно!");
+}
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+
+Div(a);
