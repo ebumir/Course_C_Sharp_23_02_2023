27bb41e632cd50430db7536f530e9c2a50be977e
Denis <denistokarev95.irk@gmail.com>
2023-03-03 16:10:01 +0800

    ClassWork 2_1 added

---
+++ Lesson2/2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Привет!");
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num % 10;
+}
+
+int result = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine(result);
+// int num = new Random().Next(100, 1000);
+ 
+// Console.WriteLine(num);
+// Console.WriteLine(num % 10 );
