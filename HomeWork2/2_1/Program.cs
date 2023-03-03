760bbb7b664accecbd03e4ca898c464732f802cc
Denis <denistokarev95.irk@gmail.com>
2023-03-03 19:42:36 +0800

    HomeWork 2_1 added

---
+++ HomeWork2/2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    int num1 = num / 10;
+    int num2 = num1 % 10;
+    return num2;
+
+}
+int result = TakeNum(new Random().Next(100,1000));
+Console.WriteLine($"Второе число: {result}");
