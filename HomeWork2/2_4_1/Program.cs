1722015115059ad4502c4bdbfdba01e4612caec9
Denis <denistokarev95.irk@gmail.com>
2023-03-04 13:14:59 +0800

    HomeWork 2_4_1 added

---
+++ HomeWork2/2_4_1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+void Week(int num1)
+{
+    if (num1>0)
+    {
+        int num = num1 % 7;
+        if (num>0 && num<6)
+        {
+            Console.WriteLine("working days");
+        }
+        else if (num ==0)
+        {
+            Console.WriteLine("Weekend!!!");
+        }
+        else if (num >5)
+        {
+            Console.WriteLine("Weekend!!!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Число должно быть больше 0!");
+    }
+}
+
+Week(a);
