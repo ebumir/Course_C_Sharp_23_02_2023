c09ce1c5346f0656e5e76e8a46f8ff619b324bbb
Denis <denistokarev95.irk@gmail.com>
2023-03-04 13:40:32 +0800

    HomeWork 2_1 Fixed

--- HomeWork2/2_1/Program.cs
+++ HomeWork2/2_1/Program.cs
@@ -1,11 +1,25 @@
 ﻿
+Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine()!);
 int TakeNum(int num)
 {
-    Console.WriteLine(num);
-    int num1 = num / 10;
-    int num2 = num1 % 10;
+            int num1 = num / 10;
+            int num2 = num1 % 10;
+        
     return num2;
 
 }
-int result = TakeNum(new Random().Next(100,1000));
-Console.WriteLine($"Второе число: {result}");
+if (a>0 && a>99 && a < 1000)
+{
+    int result=TakeNum(a);
+    Console.WriteLine($"Второе число: {result}");
+}
+else if (a<0 && a<-99 && a > -1000)
+{
+    int result=TakeNum(a);
+    Console.WriteLine($"Второе число: {result}");
+}
+else
+{
+    Console.WriteLine("Число не является трехзначным");
+}
