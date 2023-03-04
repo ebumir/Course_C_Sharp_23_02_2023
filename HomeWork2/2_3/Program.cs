6c4f17a28ca5e6063a04b5c0f4b0c0afce566036
Denis <denistokarev95.irk@gmail.com>
2023-03-04 12:22:38 +0800

    HomeWork 2_3 added

---
+++ HomeWork2/2_3/Program.cs
@@ -0,0 +1,36 @@
+﻿int TakeNum(int num)
+{
+    if (num>=0)
+    {
+        if (num>=100)
+        {
+            while (num>999)
+            
+            num = num /10;
+            
+        }
+        else 
+        {
+        Console.WriteLine("Третьей цифры нет!");
+        }
+    }
+    else
+    {
+        if (num<=-100)
+        {
+            while (num<-999)
+            num = num/10;
+        }
+        else 
+        {
+        Console.WriteLine("Третьей цифры нет!");
+        }
+    }
+    
+    int num1= num % 10;
+    return num1;
+}
+Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine()!);
+int result = TakeNum(a);
+Console.WriteLine(result);
