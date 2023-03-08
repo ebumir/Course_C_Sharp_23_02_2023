174ad97efb92370992979d46a0fdd2fe3c456712
Denis <denistokarev95.irk@gmail.com>
2023-03-08 15:07:44 +0800

    Lesson Task 3_2 added

---
+++ Lesson3/3_2/Program.cs
@@ -0,0 +1,16 @@
+﻿void Qwartes( int x)
+{
+    if (x==1)
+        Console.WriteLine("x > 0 ; y > 0");
+    else if (x ==2)
+        Console.WriteLine("x < 0 ; y > 0");
+    else if (x ==3)
+        Console.WriteLine("x < 0 ; y < 0");
+    else if (x ==4)
+        Console.WriteLine("x > 0 ; y < 0");
+    else 
+        Console.WriteLine("Err");
+}
+Console.WriteLine("Введите Четверть:");
+int a = int.Parse(Console.ReadLine()!);
+Qwartes(a);
