8715f89e4863642d790e191b610b4135709c36fe
Denis <denistokarev95.irk@gmail.com>
2023-03-08 15:53:16 +0800

    Lesson Task 3_4 added

---
+++ Lesson3/3_4/Program.cs
@@ -0,0 +1,26 @@
+﻿void Div( int num)
+{
+    int i = 1;
+    if (num > 0)
+    {
+        while (i<=num)
+        {
+            Console.Write($"{Math.Pow(i,2)}  ");
+            i++;
+        }
+    }
+    else if (num<0)
+    {
+        while (i>=num)
+        {
+            Console.Write($"{Math.Pow(i,2)}  ");
+            i--;
+        }
+    }
+    else 
+        Console.WriteLine("Err");
+}
+Console.WriteLine("Enter a Number");
+int a = int.Parse(Console.ReadLine()!);
+Div(a);
+
