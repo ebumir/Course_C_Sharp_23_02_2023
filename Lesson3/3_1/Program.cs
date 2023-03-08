4703d3b7ed88eb7de88cab72fdbf87b350be08f5
Denis <denistokarev95.irk@gmail.com>
2023-03-08 14:38:07 +0800

    Lesson Task 3_1 added

---
+++ Lesson3/3_1/Program.cs
@@ -0,0 +1,18 @@
+﻿void Qwartes( int x, int y)
+{
+    if (x== 0 || y ==0)
+        Console.WriteLine("Ошибка X или Y равны 0");
+    else if (x > 0 && y > 0)
+        Console.WriteLine("I");
+    else if (x < 0 && y > 0)
+        Console.WriteLine("II");
+    else if (x < 0 && y < 0)
+        Console.WriteLine("III");
+    else if (x > 0 && y < 0)
+        Console.WriteLine("IV");
+}
+Console.WriteLine("Введите Х:");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите Y:");
+int b = int.Parse(Console.ReadLine()!);
+Qwartes(a,b);
