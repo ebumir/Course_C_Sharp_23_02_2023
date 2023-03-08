2a4830a3e0ca07417be329fe845420c1672b4662
Denis <denistokarev95.irk@gmail.com>
2023-03-08 18:23:36 +0800

    HW Task 3_3 added

---
+++ HomeWork3/3_3/Program.cs
@@ -0,0 +1,27 @@
+﻿void Num (int num)
+{
+    int i =1;
+    if (num>0)
+    {
+        while (i<=num)
+        {
+            Console.Write($"{Math.Pow(i,3)} ");
+            i++;
+        }
+    }
+    else if (num<0)
+    {
+        while (num<=i)
+        {
+            Console.Write($"{Math.Pow(num,3)} ");
+            num++;
+        }
+    }
+    else 
+    {
+        Console.Write("0 1");
+    }
+}
+Console.WriteLine("Enter a Number:");
+int a = int.Parse(Console.ReadLine()!);
+Num(a);
