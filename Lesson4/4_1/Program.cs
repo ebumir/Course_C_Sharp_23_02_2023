d84f7fe513b7630016984cd862af5868517211e4
Denis <denistokarev95.irk@gmail.com>
2023-03-10 15:25:53 +0800

    Task 4_2 added

---
+++ Lesson4/4_1/Program.cs
@@ -0,0 +1,12 @@
+﻿int SumNum(int num)
+{
+    int sum = 0;
+
+    for (int i=1; i<=num; i++)
+        sum +=i;
+    return sum;
+}
+Console.WriteLine("Enter a Number");
+int a = int.Parse(Console.ReadLine()!);
+int result = SumNum(a);
+Console.WriteLine(result);
---
+++ Lesson4/4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿int LengthNum( int num)
+{
+    int a = 0;
+    for(int i = 1; num>0 ; i++)
+    {
+        num/=10;
+        a++;
+        
+    }
+    return a;
+}
+
+Console.WriteLine("Enter a Number");
+int x = int.Parse(Console.ReadLine()!);
+int length = LengthNum(x);
+Console.WriteLine(length);
