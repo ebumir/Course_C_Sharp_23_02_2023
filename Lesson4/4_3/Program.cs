d120c82d7459fe832a034fc9687fed3648b9e493
Denis <denistokarev95.irk@gmail.com>
2023-03-11 16:17:33 +0800

    Task 4_3 added

---
+++ Lesson4/4_3/Program.cs
@@ -0,0 +1,11 @@
+﻿int Multiply(int num)
+{
+    int a = 1;
+    for (int i = 1; i <= num; i++)
+    a*=i;
+    return a;
+}
+Console.WriteLine("enter a number:");
+int x = int.Parse(Console.ReadLine()!);
+int result = Multiply(x);
+Console.WriteLine(result);
