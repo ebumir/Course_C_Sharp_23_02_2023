8f7817d24314d9c433280929a30d32903ad33490
Denis <denistokarev95.irk@gmail.com>
2023-03-11 16:56:24 +0800

    Task 4_4 added

---
+++ Lesson4/4_4/Program.cs
@@ -0,0 +1,11 @@
+﻿void Massive( int mas)
+{
+    int[] num = new int [mas];
+    for(int i = 0; i<mas; i++)
+    {
+        num[i] = new Random().Next(2);
+        Console.Write($"{num[i]} ");
+    }
+}
+
+Massive(8);
