db8e961c74cbecc9da0ba05c15ce1eaadaeed468
Denis <denistokarev95.irk@gmail.com>
2023-03-24 12:13:11 +0800

    Lesson6 Task 6_3 Added

---
+++ Lesson6/6_3/Program.cs
@@ -0,0 +1,14 @@
+﻿void Fib(int a)
+{
+    int First = 0;
+    int Second= 1;
+    for (int i = 0; i<a;i++)
+    {
+      Console.Write(First + " ");
+      (First, Second) = (Second, First+Second); 
+    }
+}
+
+Console.WriteLine("enter a number:");
+int x = int.Parse(Console.ReadLine()!);
+Fib(x);
