249cdc845c00f385f37fb7f089dcc57de8767da7
Denis <denistokarev95.irk@gmail.com>
2023-03-24 14:31:46 +0800

    Lesson6 Task 6_4 Added

---
+++ Lesson6/6_4/Program.cs
@@ -0,0 +1,53 @@
+﻿void LengthMass(int[,] arr)
+{
+    Console.WriteLine();
+    for (int i=0; i< arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i,j]} ");
+    Console.WriteLine();
+    }
+}
+
+
+int[,] EigthMass(int row, int columns, int from, int to)
+{
+  int[,] arr = new int [row,columns];
+    for(int i = 0; i<row; i++)
+    {
+        for(int j =0; j<columns; j++)
+            arr[i,j] = new Random().Next(from,to);
+       
+    }
+    return arr;
+}
+int[,] CopyMass(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int columns = arr.GetLength(1);
+    int [,] newArray = new int[row,columns]; 
+    Console.WriteLine();
+    for (int i=0; i< row; i++)
+    {
+        for (int j = 0; j < columns; j++)
+           newArray [i,j] = arr [i,j];
+
+    }
+    return newArray;
+}
+
+
+Console.WriteLine("enter the number of rows:");
+int  RowQuantity= int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter the number of columns:");
+int  ColumnsQuantity= int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the min value of the array element:");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the max value of the array element:");
+int max = int.Parse(Console.ReadLine()!);
+
+int[,] mass= EigthMass(RowQuantity, ColumnsQuantity, min, max);
+LengthMass(mass);
+
+int[,] newMass= CopyMass(mass);
+LengthMass(newMass);
