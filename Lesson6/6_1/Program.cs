b82c4a678fe4e4ce0d358790d8cd6aaecb8cd1ee
Denis <denistokarev95.irk@gmail.com>
2023-03-24 11:47:03 +0800

    Lesson6 Task 6_1, 6_2 Added

---
+++ Lesson6/6_1/Program.cs
@@ -0,0 +1,14 @@
+﻿void Triangle(int a, int b, int c)
+{
+    if ((a < b + c) && (b < a + c) && (c < a + b))
+        Console.WriteLine("Yes!");
+    else
+        Console.WriteLine("No!");
+}
+Console.WriteLine("enter the length of side A:");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter the length of side B:");
+int y = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter the length of side C:");
+int z = int.Parse(Console.ReadLine()!);
+Triangle(x, y, z);
---
+++ Lesson6/6_2/Program.cs
@@ -0,0 +1,15 @@
+﻿string ConvertBin(int a)
+{
+    string result = "";
+    while (a > 0)
+    {
+        result = a % 2 + result;
+        a /= 2;
+    }
+    return result;
+}
+
+Console.WriteLine("enter a number:");
+int x = int.Parse(Console.ReadLine()!);
+string bin = ConvertBin(x);
+Console.WriteLine(bin);
