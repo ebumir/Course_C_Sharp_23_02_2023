b37ab29fe533602af7294360d20a5f1a06dedbc6
Denis <denistokarev95.irk@gmail.com>
2023-03-28 14:45:55 +0800

    HomeWork7 Task 7_3 Added

---
+++ Lesson7/7_3/Program.cs
@@ -0,0 +1,51 @@
+﻿void LengthMass(int[,] arr)
+{
+    Console.WriteLine();
+    for (int i=0; i< arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i,j]} ");
+    Console.WriteLine();
+    }
+}
+
+
+int[,] EigthMass(int row, int columns, int from, int to)
+{
+  int[,] arr = new int [row,columns];
+    for(int i = 0; i<row; i++)
+    {
+        for(int j =0; j<columns; j++)
+            arr[i,j] = new Random().Next(from,to+1);
+       
+    }
+    return arr;
+}
+
+int SumPos(int[,] arr)
+{
+    int sum = 0;
+    int row = arr.GetLength(0);
+    int columns = arr.GetLength(1);
+    for(int i = 0; i<row; i++)
+        for(int j =0; j<columns; j++)
+            if (i==j)
+                sum += arr[i,j];
+    return sum;
+    
+}
+
+Console.WriteLine("enter the number of rows:");
+int  RowQuantity= int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter the number of columns:");
+int  ColumnsQuantity= int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the min value of the array element:");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the max value of the array element:");
+int max = int.Parse(Console.ReadLine()!);
+
+int[,] mass= EigthMass(RowQuantity, ColumnsQuantity, min, max);
+LengthMass(mass);
+
+int result = SumPos(mass);
+Console.WriteLine("Sum of leading diagonal: {result}");
