99c637ab75505d793ddd5a6a836e1bb349b63b86
Denis <denistokarev95.irk@gmail.com>
2023-03-28 14:13:28 +0800

    Lseeson7 task 7_1 added

---
+++ Lesson7/7_1/Program.cs
@@ -0,0 +1,32 @@
+﻿void LengthMass(int[,] arr)
+{
+    Console.WriteLine();
+    for (int i=0; i< arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i,j]} ");
+    Console.WriteLine();
+    }
+}
+
+
+int[,] EigthMass(int row, int columns)
+{
+  int[,] arr = new int [row,columns];
+    for(int i = 0; i<row; i++)
+    {
+        for(int j =0; j<columns; j++)
+            arr[i,j] = i+j;
+       
+    }
+    return arr;
+}
+
+Console.WriteLine("enter the number of rows:");
+int  RowQuantity= int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter the number of columns:");
+int  ColumnsQuantity= int.Parse(Console.ReadLine()!);
+
+
+int[,] mass= EigthMass(RowQuantity, ColumnsQuantity);
+LengthMass(mass);
