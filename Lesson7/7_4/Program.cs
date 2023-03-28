14faa8ce4b4c5818368a09dbb9034080c9db75cf
Denis <denistokarev95.irk@gmail.com>
2023-03-28 15:08:21 +0800

    HomeWork7 Task 7_4 Added

---
+++ Lesson7/7_4/Program.cs
@@ -0,0 +1,49 @@
+﻿void LengthMass(int[,] arr)
+{
+    Console.WriteLine();
+    for (int i=0; i< arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i,j]} ");
+    Console.WriteLine();
+    }
+}
+
+
+int[,] EigthMass(int row, int columns, int from, int to)
+{
+  int[,] arr = new int [row,columns];
+    for(int i = 0; i<row; i++)
+    {
+        for(int j =0; j<columns; j++)
+            arr[i,j] = new Random().Next(from,to+1);
+       
+    }
+    return arr;
+}
+
+void FindIndex(int[,] arr)
+{
+    Console.WriteLine("enter a number:");
+    int num = int.Parse(Console.ReadLine()!);
+    for (int i = 0; i < arr.GetLength(0); i++)
+        for (int j = 0; j < arr.GetLength(1); j++)
+            if(arr[i,j]==num)
+            {
+                Console.WriteLine($"the number is in position[{i},{j}]");
+                return;         
+            }
+    System.Console.WriteLine("the number is not in the matrix");
+}
+Console.WriteLine("enter the number of rows:");
+int  RowQuantity= int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter the number of columns:");
+int  ColumnsQuantity= int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the min value of the array element:");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the max value of the array element:");
+int max = int.Parse(Console.ReadLine()!);
+
+int[,] mass= EigthMass(RowQuantity, ColumnsQuantity, min, max);
+LengthMass(mass);
+FindIndex(mass);
