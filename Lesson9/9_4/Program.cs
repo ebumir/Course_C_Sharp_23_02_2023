6308929712d073c6293ca926fcd713a443c2ad6e
Denis <denistokarev95.irk@gmail.com>
2023-04-09 15:45:19 +0800

    Lesson 9 task added

---
+++ Lesson9/9_4/Program.cs
@@ -0,0 +1,13 @@
+﻿int PowNums(int a, int b)
+{
+    if (b == 0) return 1;
+    return a * PowNums(a, b - 1);
+
+}
+Console.WriteLine("enter first the number:");
+int Number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter second the number:");
+int Number2 = int.Parse(Console.ReadLine()!);
+
+int res = PowNums(Number1, Number2);
+System.Console.WriteLine(res);
