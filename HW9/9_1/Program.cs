896fa33d34f8f03da5e0434a71a1d6311c2b513b
Denis <denistokarev95.irk@gmail.com>
2023-04-09 16:10:01 +0800

    HW9 Task 9_1 Added

---
+++ HW9/9_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные
+//натуральные числа в промежутке от M до N с помощью рекурсии.
+
+void EvenFromTo(int a, int b)
+{
+    if (b<a) return;
+    if (b%2==1)
+    {
+        b-=1;
+        EvenFromTo(a,b-2);
+    }
+    else 
+    EvenFromTo(a,b-2);
+
+        
+    System.Console.Write($"{b}; ");
+           
+    
+}
+
+Console.WriteLine("enter first the number:");
+int Number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter second the number:");
+int Number2 = int.Parse(Console.ReadLine()!);
+
+EvenFromTo(Number1,Number2);
